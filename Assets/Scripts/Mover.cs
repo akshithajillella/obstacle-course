2b2ca81b99fb8572e377f0d3bd52ee34519ca715
Akshitha Jillella <jillella.akshitha@gmail.com>
2022-02-03 22:13:42 +0530

    version 1

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("This is an obstacle course game");
+        Debug.Log("Move the player using WASD or the arrow keys");
+        Debug.Log("Try not to hit the walls or the objects. Good Luck!!");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0, zValue);
+    }
+}
